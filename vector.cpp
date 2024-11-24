ec85f84a6816dfc9516b0051727a2b650c4e1d8b
uphowshelet <babanskynikita@yandex.ru>
2024-11-25 01:11:47 +0700

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -52,4 +52,53 @@
 //     cout<<c;
 // }
 
+// // Напиши программу, которая считывает целые числа в вектор, удаляет все дубликаты, а затем сортирует вектор по убыванию.
+// #include<iostream>
+// #include<vector>
+// #include<algorithm>
+// using namespace std;
+// int main(){
+//     vector<int>v;
+//     int a,b;
+//     cout<<"введите размер вектора: "<<endl;
+//     cin>>b;
+//     cout<<"вводите элементы вектора: "<<endl;
+//     for(int i=0;i<b;i++){
+//         cin>>a;
+//         v.push_back(a);
+//     }
+//     sort(v.begin(),v.end());
+//     auto last = unique(v.begin(), v.end());
+//     v.erase(last, v.end());
+//     sort(v.rbegin(),v.rend());
+//     cout<<"отсортированный вектор: "<<endl;
+//     for(int i=0;i<size(v);i++){
+//         cout<<v[i]<<" ";
+//     }
+// }
 
+// //  Напиши программу, которая считывает целые числа в вектор и два индекса. 
+// // Программа должна вычислить сумму элементов вектора между этими двумя индексами (включая элементы на этих индексах).
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main(){
+//     vector<int>v;
+//     int sum=0;
+//     int a,b,c,d;
+//     cout<<"введите размер вектора: "<<endl;
+//     cin>>a;
+//     cout<<"вводите элементы вектора: "<<endl;
+//     for(int i =0;i<a;i++){
+//         cin>>b;
+//         v.push_back(b);
+//     }
+//     do{
+//         cout<<"введите диапазон для подсчета суммы: "<<endl;
+//         cin>>c>>d;
+//     }while(c>d);
+//     for(int i=c-1;i<d;i++){
+//         sum+=v[i];
+//     }
+//     cout<<"сумма элементов("<<c<<" "<<d<<"): "<<sum;
+// }
