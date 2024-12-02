03b88b0552b5e9d371721329a4a124d5df32262e
uphowshelet <babanskynikita@yandex.ru>
2024-12-02 16:23:45 +0700

    Update class.cpp

--- class.cpp
+++ class.cpp
@@ -144,3 +144,86 @@
 //     s1.setHeight(b);
 //     cout<<"S = "<<s1.f();
 // }
+
+// создайте структуру и класс "день", для каждого дня должны быть случайно сгенерированы-
+// 1)Температура(от -30 до 40)
+// 2)скорость ветра(от 0 до 30)
+// 3)тип осадка (дождь, снег, без осадков)
+// -----------------
+// заполните вектор или массив(из "дней"),размером 30 элементов
+// -----------------
+// расчитайте - среднюю температуру за месяцб максимальную и минимальную температуру
+// кол-во дождевых дней
+#include<iostream>
+#include<vector>
+#include<ctime>
+#include<string>
+#include<algorithm>
+using namespace std;
+class day{
+    private:
+    int t;
+    int speed;
+    string o;
+    int a;
+    public:
+    day(){
+        t=rand()%(40-(-30)+1)+(-30);
+        speed=rand()%(30-(-0)+1)+(-0);
+        int a= rand()%3;
+        if(a==0 and t<0){
+            o="снег";
+        }
+        else if(a==1 and t>0){
+            o="дождь";
+        }
+        else {
+            o="без осадков";
+        }
+    }
+    int getT(){
+        return t;
+    }
+    int getSpeed(){
+        return speed;
+    }
+    string getO(){
+        return o;
+    }
+};
+int main(){
+    float sum=0;
+    int c,n;
+    int k=0;
+    vector<day>v(30);
+    vector<int>g;
+    srand(time(0));
+    for (int i = 0; i < 30; i++) {
+        cout << "день " << i + 1 << ": ";
+        cout << "температура: " << v[i].getT() <<endl;
+        cout << "скорость ветра: " << v[i].getSpeed() <<endl;
+        cout << "тип осадков: " << v[i].getO() << endl;
+    }
+    cout<<endl;
+    for(int i = 0; i < 30; i++){
+        sum+=v[i].getT();
+    }
+    for(int i = 0; i < 30; i++){
+        g.push_back(v[i].getT());
+    }
+    c=*max_element(g.begin(),g.end());
+    n=*min_element(g.begin(),g.end());
+    
+    for(int i = 0;i<30;i++){
+        if(v[i].getO()=="дождь"){
+            k++;
+        }
+        else{
+            k=k-0;
+        }
+    }
+    cout<<"средняя temp: "<<sum/30<<endl;
+    cout<<"min temp: "<<n<<endl;
+    cout<<"max temp: "<<c<<endl;
+    cout<<"кол-во дождевых дней: "<<k;
+}
